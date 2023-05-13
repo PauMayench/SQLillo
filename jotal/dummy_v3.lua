9435145886b03fa74d9ca12b918504f0d3c430a4
jotal-fib <jordi.otal@estudiantat.upc.edu>
2023-05-13 16:44:10 +0200

    v3 a mitges

--- jotal/dummy_v3.lua
+++ jotal/dummy_v3.lua
@@ -9,39 +9,22 @@ end
 -- Main bot function
 function bot_main(me)
     
-    local me_pos = me:pos() -- Returns a vec type with your player position
-    
-    -- Update cooldowns
-    for i = 1, 3 do
-        if cooldowns[i] > 0 then
-            cooldowns[i] = cooldowns[i] - 1
-        end
-    end
-
     -- Set target to closest visible enemy PLAYER NOT BULLET
     local closest_enemy = closest_enemy_player(me)
     local target = closest_enemy[1]
-    local min_distance = closest_enemy[2]
+    local t_distance = closest_enemy[2]
 
     if target then
-        -- testing 
-        if im_closest(target, me) then
-            print("Im the closest")
-        else
-            print("I'm not")
+        local aux = attack_vector(t_distance, target, me)
+        local spell = aux[1]
+        local dir   = aux[2]
+        if spell ~= nil then
+            me:cast(spell, dir)
+            print("Dist  :", t_distance)
+            print("Spell :", spell)
+            print("Dir   :", dir)
         end
-        -- end testing
-
-        local direction = target:pos():sub(me_pos)
-        -- If target is within melee range and melee attack is not on cooldown, use melee at
-        if min_distance <= 2 and cooldowns[3] == 0 then
-            me:cast(2, direction)
-            cooldowns[3] = 50
-        -- If target is not within melee range and projectile is not on cooldown, use projec
-        elseif cooldowns[1] == 0 then
-            me:cast(0, direction)
-            cooldowns[1] = 1
-        end
+        
         -- Move towards the target
         me:move(direction)
     end
@@ -53,22 +36,43 @@ function closest_enemy_player(my_player)
     local closest_enemy = nil
     local min_distance = math.huge
     for _, player in ipairs(my_player:visible()) do
-        if player:type() == "player" then -- ADDED
-            local dist = vec.distance(me_pos, player:pos())
+        if player:type() == "player" and player:id() ~= my_player:id() then -- ADDED
+            local dist = vec.distance(my_player:pos(), player:pos())
             if dist < min_distance then
                 min_distance = dist
                 closest_enemy = player
             end
         end
     end
+    print("Calculated distance :", min_distance)
     return {closest_enemy, min_distance}
 end
 
 -- [[
---      If I'm the closest player, shoot towards his direction
---      Else, try to predict where he's moving to and shoot
+-- SKILL SPELLS
+--   0 <- small projectile ; damage 10
+--   1 <- dash given direction
+--   2 <- melee attack ; distance 2 ; damage 20
 -- ]]
+function attack_vector(t_dist, t_player, my_player)
+    local mp = my_player
+    local direction = nil
+    local spell = nil
+    -- If target is within melee range and melee attack is not on cooldown, use melee at
+    -- UPDATED -> MELEE ATTACK
+    if t_dist <= 2 and mp:cooldown(2) == 0 then
+        spell = 2
+    -- If target is not within melee range and projectile is not on cooldown, use projec
+    -- UPDATED -> SHOOT
+    elseif mp:cooldown(0) == 0 then
+        spell = 0
+    end
+    
+    direction = t_player:pos():sub(mp:pos()) -- PROVISIONAL
+    return {spell, direction}
+end
 
+-- =========== UNUSED ===========
 -- Check if "me" is the closest to our target
 function im_closest(target_player, my_player)
     local my_id = my_player:id()
@@ -85,3 +89,7 @@ function im_closest(target_player, my_player)
     return my_id == cl_id
 end
 
+-- [[
+--      If I'm the closest player, shoot towards his direction
+--      Else, try to predict where he's moving to and shoot
+-- ]]
